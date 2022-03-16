03d510c538f465e074320ac49955fe3217c7dac2
Morningstar000 <96479756+Morningstar000@users.noreply.github.com>
2022-03-16 23:12:41 +0530

    Add files via upload

---
+++ 20CE075_practical8.py
@@ -0,0 +1,32 @@
+# 20CE075 Parth Parmar
+# Practical 8: Write a Program in Python to implement a Stack Data Structure using Class and Objects, with push, pop, and traversal method.
+
+class stack:
+    def __init__(self):
+        self.arr = []
+
+    def push(self, data):
+        self.arr.append(data)
+
+    def pop(self):
+        return self.arr.pop()
+
+    def is_empty(self):
+        return self.arr == []
+
+    def display(self):
+        if self.is_empty():
+            print("No element in stack")
+        for i in self.arr:
+            print(i, end=" ")
+        print("\n")
+
+st = stack()
+st.push(94)
+st.push(86)
+st.push(75)
+print("Number of  element in stack:")
+st.display()
+st.pop()
+print("Number of element in stack")
+st.display()
